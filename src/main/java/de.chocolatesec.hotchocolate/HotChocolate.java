bd6a75ce35cb87fc21838efc77d4fabf3d88bc71
Torben Schweren <torben@schweren.dev>
2024-05-03 00:27:23 +0200

    Set up project
     - Add gradle
      - Including java plugin
      - Including manadrop plugin
     - Add basic source code to get plugin working

---
+++ src/main/java/de.chocolatesec.hotchocolate/HotChocolate.java
@@ -0,0 +1,11 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HotChocolate extends JavaPlugin {
+    public void onEnable() {
+        getLogger().info("HotChocolate enabled!");
+    }
+
+    public void onDisable() {
+        getLogger().info("HotChocolate disabled!");
+    }
+}
---
+++ util/runBuildTools.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+CWD=$(pwd)
+
+mkdir -p /tmp/chocolatesec/buildtools
+cd /tmp/chocolatesec/buildtools
+
+curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar BuildTools.jar
+
+cd $CWD
